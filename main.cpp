1df34401de52af8605c836e5ce0eb60b584e64dc
hark130 <hark130@yahoo.com>
2017-10-22 11:31:38 -0500

    master: main focused on buggy condition testing

--- main.cpp
+++ main.cpp
@@ -3,43 +3,44 @@
 #include <iostream>
 #include <string>
 
-#define NUM_RUNS 10000
+#define NUM_RUNS 2
+// UINT_MAX == 4294967295
 
 int main(void)
 {
-	int tempInt = 0;
+	// int tempInt = 0;
 
 	StringNum someNum(std::string("This is a test"));
-	std::cout << "String is:\t" << someNum.getStringNum() << std::endl;
-	someNum.setStringNum(std::string("This is also test"));
-	std::cout << "String is:\t" << someNum.getStringNum() << std::endl;
-	someNum.setStringNum(std::string("1234"));
-	std::cout << "String is:\t" << someNum.getStringNum() << std::endl;
-	someNum.addOne();
-	std::cout << "1234 + 1 = " << someNum.getStringNum() << std::endl;
-	someNum.setStringNum(std::string("789"));
-	someNum.addOne();
-	std::cout << "789 + 1 = " << someNum.getStringNum() << std::endl;
-	someNum.setStringNum(std::string("799"));
-	someNum.addOne();
-	std::cout << "799 + 1 = " << someNum.getStringNum() << std::endl;
-	someNum.setStringNum(std::string("999"));
-	someNum.addOne();
-	std::cout << "999 + 1 = " << someNum.getStringNum() << std::endl;
+	// std::cout << "String is:\t" << someNum.getStringNum() << std::endl;
+	// someNum.setStringNum(std::string("This is also test"));
+	// std::cout << "String is:\t" << someNum.getStringNum() << std::endl;
+	// someNum.setStringNum(std::string("1234"));
+	// std::cout << "String is:\t" << someNum.getStringNum() << std::endl;
+	// someNum.addOne();
+	// std::cout << "1234 + 1 = " << someNum.getStringNum() << std::endl;
+	// someNum.setStringNum(std::string("789"));
+	// someNum.addOne();
+	// std::cout << "789 + 1 = " << someNum.getStringNum() << std::endl;
+	// someNum.setStringNum(std::string("799"));
+	// someNum.addOne();
+	// std::cout << "799 + 1 = " << someNum.getStringNum() << std::endl;
+	// someNum.setStringNum(std::string("999"));
+	// someNum.addOne();
+	// std::cout << "999 + 1 = " << someNum.getStringNum() << std::endl;
 
-	someNum.setStringNum(std::string("0"));
-	for (int i = 1; i <= 10; ++i)
-	{
-		std::cout << someNum.getStringNum() << " + " << i << " = ";
-		someNum.addNum(i);
-		std::cout << someNum.getStringNum() << std::endl;
-	}
+	// someNum.setStringNum(std::string("0"));
+	// for (int i = 1; i <= 10; ++i)
+	// {
+	// 	std::cout << someNum.getStringNum() << " + " << i << " = ";
+	// 	someNum.addNum(i);
+	// 	std::cout << someNum.getStringNum() << std::endl;
+	// }
 
-	tempInt = 1234567890;
-	someNum.setStringNum(std::string("1234567890"));
-	std::cout << someNum.getStringNum() << " + " << tempInt << " = ";
-	someNum.addNum(tempInt);
-	std::cout << someNum.getStringNum() << std::endl;
+	// tempInt = 1234567890;
+	// someNum.setStringNum(std::string("1234567890"));
+	// std::cout << someNum.getStringNum() << " + " << tempInt << " = ";
+	// someNum.addNum(tempInt);
+	// std::cout << someNum.getStringNum() << std::endl;
 
 	someNum.setStringNum(std::string("0"));
 	std::cout << "STARTING TEST" << std::endl;
@@ -52,13 +53,17 @@ int main(void)
 		///////////////////////////////////////////////////////
 		// .addNum() doesn't work properly for values > 9!!! //
 		///////////////////////////////////////////////////////
-		someNum.addNum(9);
+		// someNum.addNum(9);
+		// someNum.addNum(10);
+		// someNum.addNum(UINT_MAX);
+		std::cout << "UINT_MAX / 2 == " << UINT_MAX / 2 << std::endl;  // DEBUGGING
+		someNum.addNum(UINT_MAX / 2);
 		newNum = stoll(someNum.getStringNum());
 		std::cout << "oldNum:\t" << oldNum << std::endl;
 		std::cout << "newNum:\t" << newNum << std::endl;
 		if (newNum <= oldNum)
 		{
-			std::cout << "oldNum > newNum!!!" << std::endl;
+			std::cout << "oldNum >= newNum!!!" << std::endl;
 			break;
 		}
 	}
